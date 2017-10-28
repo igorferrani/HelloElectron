4dc91af1441cb3d4f1bae05a53c9c61f5d329d06
Igor Ferrani <igor.ferrani@gmail.com>
2017-10-28 04:10:59 -0200

    Test shortcut 'CommandOrControl+X'

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
 const electron = require('electron')
-const {app, BrowserWindow} = electron
+const {app, BrowserWindow, globalShortcut} = electron
 
 const path = require('path')
 const url  = require('url')
@@ -9,14 +9,31 @@ const url  = require('url')
 let win
 
 function createWindow(){
-	win = new BrowserWindow ({width: 800, height:600})
+	win = new BrowserWindow ({width: 1120, height:600})
 	win.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
 		protocol: 'file',
 		slashes: true
 	}))
 
+  // Register a 'CommandOrControl+X' shortcut listener.
+  const ret = globalShortcut.register('CommandOrControl+X', () => {
+    console.log('CommandOrControl+X is pressed')
+  })
+
+  if (!ret) {
+    console.log('registration failed')
+  }
+
+  // Check whether a shortcut is registered.
+  console.log(globalShortcut.isRegistered('CommandOrControl+X'))
+
 	win.webContents.openDevTools()
+}
+
+exports.shortcut = () => {
+  console.log("oi")
+
 }
 
 exports.openWindow = () => {
