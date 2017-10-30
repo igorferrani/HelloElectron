3e09b3435cb9d8aa163ccfc262d76d9d92952874
Igor Ferrani <igor.ferrani@gmail.com>
2017-10-30 21:56:46 -0200

    arquivo csv

---
+++ public/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    to aqui
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
--- public/main.js
+++ public/main.js
@@ -1,5 +1,3 @@
-
-
 const electron = require('electron')
 const {app, BrowserWindow, globalShortcut} = electron
 
@@ -15,26 +13,13 @@ function createWindow(){
 		protocol: 'file',
 		slashes: true
 	}))
-
-  // Register a 'CommandOrControl+X' shortcut listener.
-  const ret = globalShortcut.register('CommandOrControl+X', () => {
-    console.log('CommandOrControl+X is pressed')
-  })
-
-  if (!ret) {
-    console.log('registration failed')
-  }
-
-  // Check whether a shortcut is registered.
-  console.log(globalShortcut.isRegistered('CommandOrControl+X'))
-
-	//win.webContents.openDevTools()
+	win.webContents.openDevTools()
 }
 
 exports.openWindow = () => {
 	let newWin = new BrowserWindow ({width: 400, height:200})
 	newWin.loadURL(url.format({
-		pathname: path.join(__dirname, 'enupal.html'),
+		pathname: path.join(__dirname, '/enupal.html'),
 		protocol: 'file',
 		slashes: true
 	}))
