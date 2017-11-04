83b78cdd3ff6435a8c2d4de404e9885a2898716f
Igor Ferrani <igor.ferrani@gmail.com>
2017-11-04 01:14:00 -0200

    Save options

--- public/assets/js/model/service.js
+++ public/assets/js/model/service.js
@@ -1,7 +1,7 @@
 function Service (service) {
   this.url = service.url || ''
   this.request = service.request || false
-  this.status = service.status || ''
+  this.status = service.status || -1
 }
 
 module.exports = Service
